fd0b1cf18959b5e76da4eaccad6b28996306599e
Sami Ahmed <ali123ali123aliali123771@gmail.com>
2026-01-16 03:20:38 +0300

    fix: clean main.dart and remove the brocken imports

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_application_9/components/botton_page.dart';
 import 'design/app_theme.dart';
-import 'screens/demo_screen.dart';
-import 'screens/theme_playground_screen.dart';
 
 void main() {
   runApp(const MyApp());
@@ -16,8 +13,8 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
       theme: AppTheme.lightTheme,
-      home: const ThemePlaygroundScreen(),
-      routes: {'botonpage': (context) => BottonPage()},
+      home: const Scaffold(),
+      routes: {},
     );
   }
 }
