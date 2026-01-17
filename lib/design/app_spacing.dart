aab82a668d77829dab51db50e00aa188b232ac40
Sami Ahmed <ali123ali123aliali123771@gmail.com>
2026-01-17 22:48:27 +0300

    chore: remove db smoke test from main

--- lib/design/app_spacing.dart
+++
@@ -1,9 +0,0 @@
-/// كل المسافات القياسية للتطبيق (Base 8pt Grid)
-abstract class AppSpacing {
-  static const double xs = 4;
-  static const double sm = 8;
-  static const double md = 16;
-  static const double lg = 24;
-  static const double xl = 32;
-  static const double xxl = 40;
-}
